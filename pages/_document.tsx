65132561e2054c1ca67ed156e9f1e3526c38a2e4
Azraf Taohid <38202129+azraftaohid@users.noreply.github.com>
2022-03-23 19:57:13 +0600

    - fixed icons

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,9 +8,9 @@ export class AppDocument extends Document {
 				<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
 				<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
 				<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-				<link rel="icon" type="image/svg+xml" sizes="any" href="/favicon.svg" />
+				<link rel="icon" type="image/svg+xml" sizes="any" href="/vector.svg" />
 				<link rel="shortcut icon" type="image/svg+xml" href="/vector.svg" />
-				<link rel="shortcut icon" type="image/png" href="/favicon-32x32.svg" />
+				<link rel="shortcut icon" type="image/png" href="/favicon-32x32.png" />
 				<link rel="manifest" href="/site.webmanifest" />
 				<link rel="mask-icon" href="/safari-pinned-tab.svg" color="#0078d4" />
 				<meta name="msapplication-TileColor" content="#2d89ef" />
