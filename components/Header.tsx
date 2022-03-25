b4f41451bf094c8c4777dfe63d825f2d7376b763
Azraf Taohid <38202129+azraftaohid@users.noreply.github.com>
2022-03-25 13:59:14 +0600

    - nav links are wrapped inside next/link

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
+import Link from "next/link";
 import { useRouter } from "next/router";
-import { useCallback } from "react";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
@@ -34,10 +34,6 @@ export const Header: React.FunctionComponent = () => {
 	const { current: theme, setTheme } = useTheme();
 
 	const toggleTheme = () => setTheme(theme === Theme.LIGHT ? Theme.DARK : Theme.LIGHT);
-	const handleNav = useCallback((key: string | null) => {
-		if (key) router.push(key);
-	}, [router]);
-
 
 	return <>
 		<Navbar id="nav" className={mergeNames(styles.main, "py-3")} expand="md" sticky="top">
@@ -52,13 +48,12 @@ export const Header: React.FunctionComponent = () => {
 				</NavbarBrand>
 				<NavbarToggle className="order-1" aria-controls="navbar-nav"/>
 				<NavbarCollapse id="navbar-nav">
-					<Nav className={mergeNames(styles.navItems, "position-md-absolute top-md-50 start-md-50 translate-middle-md")} activeKey={router.pathname} onSelect={handleNav}>
-						{navs.map(nav => <NavLink 
-							key={nav.pathname} 
-							eventKey={nav.pathname} 
-						>
-							{nav.title}
-						</NavLink>)}
+					<Nav className={mergeNames(styles.navItems, "position-md-absolute top-md-50 start-md-50 translate-middle-md")} activeKey={router.pathname}>
+						{navs.map(nav => <Link key={nav.pathname} href={nav.pathname} passHref>
+							<NavLink eventKey={nav.pathname}>
+								{nav.title}
+							</NavLink>
+						</Link>)}
 					</Nav>
 				</NavbarCollapse>
 				<Icon role="button" className={"btn btn-outline-secondary order-0 order-md-2 ms-auto me-3 me-md-0"} name="dark_mode" tabIndex={1} onClick={toggleTheme} />
