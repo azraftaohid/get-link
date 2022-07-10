96f4679c0aed675458d37cc2bcb9da932f4b5eb1
Azraf Taohid <38202129+azraftaohid@users.noreply.github.com>
2022-07-10 22:43:18 +0600

    - reverted DOMAIN logic

--- utils/urls.ts
+++ utils/urls.ts
@@ -1,5 +1,4 @@
-export const DOMAIN = (process.env.NEXT_PUBLIC_VERCEL_URL && `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`) 
-    || "https://getlink.vercel.app";
+export const DOMAIN = "https://getlink.vercel.app";
 
 export function createUrl(...segments: string[]) {
 	return createAbsoluteUrl("", ...segments);
