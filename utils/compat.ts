4b9047b546426d72dccd6e31327ae6b6439d4072
Azraf Taohid <38202129+azraftaohid@users.noreply.github.com>
2023-11-15 05:01:15 +0600

    feat: suport for appcheck

--- utils/compat.ts
+++ utils/compat.ts
@@ -1,7 +1,8 @@
-export {};
+export { };
 
 declare global {
 	interface Window {
 		clarity: (action: "identify" | "set", ...args: string[]) => void;
+		FIREBASE_APPCHECK_DEBUG_TOKEN: boolean | string;
 	}
 }
--- utils/init.ts
+++ utils/init.ts
@@ -1,3 +1,4 @@
+import { get } from "@vercel/edge-config";
 import {
 	initializeAnalytics,
 	isSupported as isAnalyticsSupported,
@@ -5,6 +6,7 @@ import {
 	setUserId,
 } from "firebase/analytics";
 import { getApps, initializeApp } from "firebase/app";
+import { CustomProvider, ReCaptchaEnterpriseProvider, initializeAppCheck } from "firebase/app-check";
 import {
 	browserLocalPersistence,
 	browserSessionPersistence,
@@ -18,7 +20,8 @@ import {
 } from "firebase/firestore/lite";
 import { connectStorageEmulator, getStorage } from "firebase/storage";
 import { acquireExperienceOptions } from "./analytics";
-import { firebaseConfig } from "./configs";
+import { hasWindow } from "./common";
+import { appcheckDebugToken, firebaseConfig, siteKey } from "./configs";
 import { FIREBASE_APP_NAME } from "./firebase";
 
 let hasInit = false;
@@ -44,6 +47,8 @@ function initFirebase() {
 	});
 
 	if (process.env.NODE_ENV === "development") {
+		if (hasWindow) self.FIREBASE_APPCHECK_DEBUG_TOKEN = appcheckDebugToken;
+		
 		connectFirestoreLiteEmulator(firestore, "localhost", 8080);
 		connectStorageEmulator(storage, "localhost", 9199);
 		connectAuthEmulator(auth, "http://localhost:9099", {
@@ -56,6 +61,37 @@ function initFirebase() {
 		console.debug = noOp;
 		console.log = noOp;
 	}
+
+	let appCheckProvider: ReCaptchaEnterpriseProvider | CustomProvider;
+	if (hasWindow) {
+		console.debug("Using recaptcha enterprise for appcheck purposes.");
+		appCheckProvider = new ReCaptchaEnterpriseProvider(siteKey);
+	} else {
+		appCheckProvider = new CustomProvider({
+			getToken: async () => {
+				console.debug("Getting appcheck token from database.");
+				const options = await get("appCheck");
+				if (!options || typeof options !== "object" || Array.isArray(options)) 
+					throw new Error("AppCheck object is malformed or missing from database.");
+
+				const token = options.token;
+				const expireTime = options.expireTime;
+
+				if (typeof token !== "string" || typeof expireTime !== "number" || expireTime < new Date().getTime())
+					throw new Error("Invalid token or expire time of AppCheck object.");
+
+				return {
+					token,
+					expireTimeMillis: expireTime,
+				};
+			}
+		});
+	}
+
+	initializeAppCheck(app, {
+		provider: appCheckProvider,
+		isTokenAutoRefreshEnabled: true,
+	});
 
 	isAnalyticsSupported().then((bool) => {
 		if (!bool) return;
