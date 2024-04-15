6acba17ce4169e8b0c60c94685bc23a702ca6376
Azraf Taohid <38202129+azraftaohid@users.noreply.github.com>
2024-04-15 19:26:12 +0600

    build: update sitemap, deprecate dynamic sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -3,6 +3,16 @@
 module.exports = {
 	siteUrl: "https://getlink.vercel.app",
 	generateRobotsTxt: true,
-	exclude: ["/v/*", "/unsubscribe"],
-	changefreq: "weekly",
+	exclude: ["/v/*", "/unsubscribe", "/f/*", "/continue/*", "/account/*", "/d"],
+	generateIndexSitemap: false,
+	transform: async (config, path) => {
+		let priority;
+		if (path === "/") priority = 1;
+		else if (path === "/about") priority = .8;
+
+		return {
+			loc: path,
+			priority: priority,
+		};
+	}
 };
