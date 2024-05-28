b007eaac1eee88707495928ff32fc048f46751e2
Azraf Taohid <38202129+azraftaohid@users.noreply.github.com>
2024-05-28 13:59:43 +0600

    fix: go back to dashboard from /f/[cfk] page
    Update hash based list to query based in dashboard to mitigate Nextjs 14 issue https://github.com/vercel/next.js/issues/56112

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -3,27 +3,24 @@
 import { RecentFiles } from "@/components/list/RecentFiles";
 import { RecentLinks } from "@/components/list/RecentLinks";
 import { RecentListPlaceholder } from "@/components/list/RecentListPlaceholder";
+import { useAppRouter } from "@/utils/useAppRouter";
 import { useUser } from "@/utils/useUser";
-import { useEffect, useState } from "react";
+import { usePathname, useSearchParams } from "next/navigation";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import ToggleButton from "react-bootstrap/ToggleButton";
 import ToggleButtonGroup from "react-bootstrap/ToggleButtonGroup";
-import { EmptyView, ErrorView, detnModeFromHash } from "./helperComponents";
+import { EmptyView, ErrorView, Mode, isValidMode } from "./helperComponents";
 
 export default function Page() {
+	const router = useAppRouter();
+	const pathname = usePathname();
+	const search = useSearchParams();
+
 	const { user, isLoading } = useUser();
-	const [mode, setMode] = useState(detnModeFromHash(typeof window !== "undefined" ? window.location.hash : "", "links"));
 
-	useEffect(() => {
-		const handler = (evt: HashChangeEvent) => {
-			const url = new URL(evt.newURL);
-			setMode(c => detnModeFromHash(url.hash, c));
-		};
-
-		window.addEventListener("hashchange", handler);
-		return () => window.removeEventListener("hashchange", handler);
-	}, []);
+	const strMode = search?.get("list");
+	const mode: Mode = isValidMode(strMode) ? strMode : "links";
 
 	return <>
 		<Row>
@@ -35,7 +32,12 @@ export default function Page() {
 					className="ms-auto"
 					name="mode-btn-radio"
 					value={mode}
-					onChange={(value) => window.location.hash = "#" + value}
+					onChange={(value) => {
+						const params = new URLSearchParams(search?.toString());
+						params.set("list", value);
+
+						router.push(pathname + "?" + params.toString());
+					}}
 					type="radio"
 				>
 					<ToggleButton id="mode-btn-links" variant="outline-secondary" value="links">
