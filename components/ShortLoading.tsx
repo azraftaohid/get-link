56f71fa555db0e52e355fef60b5b687b1280510b
Azraf Taohid <38202129+azraftaohid@users.noreply.github.com>
2024-06-13 03:11:48 +0600

    refactor: update navigation progress bar

--- components/ShortLoading.tsx
+++ components/ShortLoading.tsx
@@ -2,47 +2,34 @@ import React, { useEffect, useState } from "react";
 import ProgressBar, { ProgressBarProps } from "react-bootstrap/ProgressBar";
 import { mergeNames } from "../utils/mergeNames";
 
-function calcNext(from: number, iteration: number /* a non zero positive integer */) {
-	return from + (100 - from) / (3 * iteration);
-}
-
 export const ShortLoading: React.FunctionComponent<React.PropsWithChildren<ShortLoadingProps>> = ({
 	className,
-	striped = true,
-	animated,
+	striped = false,
+	animated = false,
 	variant,
-	initPercentage = 0,
+	initPercentage,
 	stale,
 	...rest
 }) => {
-	const [iteration, setIteration] = useState(0);
-	const [percentage, setPercentage] = useState(Math.max(0, initPercentage));
+	const [percentage, setPercentage] = useState(Math.max(0, initPercentage || 0));
 
 	useEffect(() => {
 		if (stale) return;
 
-		const setter = () => {
-			const i = iteration;
-			setIteration(c => ++c);
-			setPercentage(
-				i === 0
-					? 30
-					: (current) => {
-							const proposed = calcNext(current, i);
-							return proposed - current < 0.1 ? current : proposed;
-					  }
-			);
-		};
-
-		const delay = 3000 * (iteration / (10 + 6 * (iteration - 1)));
-		const id = setTimeout(setter, delay);
-		return () => clearTimeout(id);
-	}, [iteration, stale]);
+		const intervalId = setInterval(() => setPercentage(c => {
+			// We are using linear animation for 300ms
+			// And we target to reach 100% in 2.5s initially. After 60%, we're gradually decreasing the progress rate
+			if (c < 60) return c + 12; // multiply by 12 because 100% -> 2500ms; 1% -> 25ms; 300ms / 25ms = 12
+			return c + (((100 - c) / 100) * 12);
+		}), 300);
+		return () => clearInterval(intervalId);
+	}, [stale]);
 
 	useEffect(() => {
+		if (initPercentage === undefined) return;
+
 		const newPerc = Math.min(100, Math.max(0, initPercentage));
 		setPercentage(newPerc);
-		if (newPerc === 0) setIteration(0);
 	}, [initPercentage]);
 
 	return (
